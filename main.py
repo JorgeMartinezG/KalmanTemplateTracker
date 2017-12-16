e702325f964b1c0de897b22793686a60a9eee787
Jorge Martinez <jorgemg0327@gmail.com>
2017-12-16 09:23:47 -0500

    Updated repo

--- main.py
+++ main.py
@@ -1,21 +1,59 @@
 import cv2
+import cv2.cv as cv
 import optparse
 import os
 
-#def create_video_capture():
+#def create_cap():
 #def get_or_create_helpers(face_coords, img_frame)
 
 
+def update_params(obj, x_point, y_point):
+    kalman, measurement = obj['kalman'], obj['measurement']
 
-def detect_faces(classifier, img_frame):
-    faces_coords = classifier.detectMultiScale(
+    measurement[0, 0] = x_point
+    measurement[1, 0] = y_point
+
+    kalman.state_pre[0, 0] = x_point 
+    kalman.state_pre[1, 0] = y_point
+    kalman.state_pre[2, 0] = 0
+    kalman.state_pre[3, 0] = 0
+
+    estimates = cv.KalmanCorrect(kalman, measurement)
+
+    obj.update(dict(kalman=kalman,
+                    measurement=measurement,
+                    estimates=estimates))
+
+    return obj 
+
+
+def kalman_create():
+    kalman = cv.CreateKalman(4, 2, 0)
+
+    # set kalman transition matrix
+    kalman.transition_matrix[0, 0] = 1
+    kalman.transition_matrix[1, 1] = 1
+    kalman.transition_matrix[2, 2] = 1
+    kalman.transition_matrix[3, 3] = 1
+
+    # set Kalman Filter
+    cv.SetIdentity(kalman.measurement_matrix, cv.RealScalar(1))
+    cv.SetIdentity(kalman.process_noise_cov, cv.RealScalar(1e-5))
+    cv.SetIdentity(kalman.measurement_noise_cov, cv.RealScalar(1e-1))
+    cv.SetIdentity(kalman.error_cov_post, cv.RealScalar(0.1))
+
+    return kalman 
+
+
+def detect_objects(classifier, img_frame):
+    coords = classifier.detectMultiScale(
         img_frame,
         scaleFactor=1.1,
         minNeighbors=5,
         minSize=(30, 30),
         flags=cv2.cv.CV_HAAR_SCALE_IMAGE
     )
-    return faces_coords
+    return coords
 
 
 def draw_rectangle(img_frame, coords):
@@ -29,47 +67,113 @@ def draw_rectangle(img_frame, coords):
     )
 
 
+def create_search_patch(img, obj_bbox, search_size=50):
+    x_point, y_point, width, height = obj_bbox
+
+    roi_min_y = max(0, y_point - search_size)
+    roi_max_y = min(img.shape[1], y_point + height + search_size)
+    roi_min_x = max(0, x_point - search_size)
+    roi_max_x = min(img.shape[0], x_point + width + search_size)
+
+    roi = img[roi_min_y:roi_max_y, roi_min_x:roi_max_x]
+
+    return roi, roi_min_x, roi_min_y 
+
+
+def get_obj_patch(img, obj_bbox):
+    x_point, y_point, width, height = obj_bbox
+    return img[y_point: y_point + height + 1,
+               x_point: x_point + width + 1]
+
+
+def create_object_dict(img, obj_bbox):
+    x_point, y_point, _, _ = obj_bbox
+
+    # Create Kalman filter.
+    kalman = kalman_create()
+
+    # Create measurement matrix.
+    measurement = cv.CreateMat(2, 1, cv.CV_32FC1)
+
+    # Create image template patch.
+    obj_patch = get_obj_patch(img, obj_bbox)
+
+    # Create object dictionary.
+    obj = dict(kalman=kalman, measurement=measurement, tpl=obj_patch)
+
+    # Update kalman params.
+    obj = update_params(obj, x_point, y_point)
+
+    return obj
+
+
 def main():
     # Read input options.
     #options = parse_options()
-    video_capture = cv2.VideoCapture('gupta/c43.avi')
+    cap = cv2.VideoCapture('gupta/c01.avi')
+    track_sw = False
 
-    # Create Haar-cascade classifier
-    classifier_path = 'Classifiers/haarcascade_frontalface_alt.xml'
-    face_cascade = cv2.CascadeClassifier(classifier_path)
+    # Create detector.
+    classifier_path = 'classifiers/haarcascade_frontalface_alt.xml'
+    classifier = cv2.CascadeClassifier(classifier_path)
 
-    while 1:
+    while cap.isOpened():
         # Retreive video frame from capture and status
-        status, frame = video_capture.read()
+        _, img = cap.read()
+        objs = detect_objects(classifier, img)
+        len_objs = len(objs)
 
-        # Stop loop when the video has finished.
-        if not status:
-            break
+        #TODO: Check template match here.
+        if len_objs == 0 and not track_sw:
+            cv2.imshow('Video', img); cv2.waitKey(25)
+            continue
 
-        # Find faces using Haar cascade classifier.
-        faces = detect_faces(face_cascade, frame)
+        # Initialize Kalman filter.
+        elif len_objs != 0 and not track_sw:
+            pred_bbox = objs[0]
+            obj = create_object_dict(img, pred_bbox)
+            track_sw = True
+            continue
 
-        # Create region of interest (roi) and face patch given a coordinate.
-        for face in faces:
-            x_point, y_point, width, height = face
+        prediction = cv.KalmanPredict(obj.get('kalman'))
+        pred_bbox[:2] = prediction[0, 0], prediction[1, 0]
+        bbox = pred_bbox.copy()
 
-            # Create roi nearby detected face.
-            test_val = 50
-            roi = frame[y_point - test_val: y_point + height + test_val,
-                        x_point - test_val: x_point + width + test_val]
-            test_val = 0
-            face_patch = frame[y_point - test_val: y_point + height + test_val,
-                               x_point - test_val: x_point + width + test_val]
-            cv2.imshow('roi', roi)
-            cv2.imshow('face_patch', face_patch)
+        if len_objs == 0:
+            # Create neighborhood around kalman prediction. 
+            search_patch, min_x, min_y = create_search_patch(img, pred_bbox)
+            template = obj.get('tpl')
+            height, width, _ = template.shape
 
-        #roi, patch = get_or_create_helpers(faces, frame)
-        
-        # Draw a rectangle around the faces
-        for face_coords in faces:
-            draw_rectangle(frame, face_coords)
+            # Run template match.
+            res = cv2.matchTemplate(search_patch, template, cv2.TM_CCOEFF)
+            _, _, _, top_left = cv2.minMaxLoc(res)
 
-        cv2.imshow('Video', frame)
+            # Add the top left position from the search patch.
+
+            top_left = top_left[0] + min_x, top_left[1] + min_y
+            bottom_right = (top_left[0] + width, top_left[1] + height)
+            cv2.rectangle(img, top_left, bottom_right, (0, 255, 0), 2)
+
+
+        if len_objs != 0:
+            full_update = True
+            # TODO: Check if bounding box distance to prediction is below th.
+            out_bbox = objs[0]
+            obj = update_params(obj, out_bbox[0], out_bbox[1])
+
+            estimates = obj.get('estimates')
+            bbox = [estimates[0, 0], estimates[1,0]]
+            bbox.extend(pred_bbox[2:].tolist())
+
+            # Update template img.
+            img_tpl = get_obj_patch(img, out_bbox) 
+            obj.update(dict(tpl=img_tpl))
+
+            bbox = [int(coord) for coord in bbox]
+            draw_rectangle(img, bbox)
+
+        cv2.imshow('video', img)
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
 
